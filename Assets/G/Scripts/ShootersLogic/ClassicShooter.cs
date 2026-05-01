990a4b82e9385ae00b7f4d50b16cceb72c59498e
Velsimir <nintendocoin@yandex.ru>
2026-05-01 21:02:12 +0300

    Add shooter logic

---
+++ Assets/G/Scripts/ShootersLogic/ClassicShooter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using G.Scripts.BulletLogic;
+using G.Scripts.Services.Spawner;
+using G.Scripts.Services.Update;
+using G.Scripts.ShootersLogic;
+using UnityEngine;
+
+namespace G.Scripts.PlayerLogic
+{
+    public class ClassicShooter : IShooter
+    {
+        private ISpawnerService _spawnerService;
+        private Bullet _bulletPrefab;
+        private float _lastShootTime;
+        private Transform _shootPosition;
+        
+        private readonly float _timeBetweenShoot = 2f;
+        
+        public ClassicShooter()
+        {
+            _shootPosition = G.Instance.Player.ClassicShootPoint;
+            
+            _spawnerService = G.Instance.Services.GetService<ISpawnerService>();
+            _bulletPrefab = G.Instance.Bullets.ClassicBulletPrefab;
+            
+            G.Instance.Services.GetService<IUpdateService>().AddNew(this);
+        }
+
+        public void Shoot()
+        {
+            _spawnerService.Spawn(_bulletPrefab, _shootPosition, null);
+        }
+
+        public void FixedUpdate(float deltaTime)
+        { }
+
+        public void Update(float deltaTime)
+        {
+            if (_lastShootTime >= _timeBetweenShoot)
+            {
+                Shoot();
+                _lastShootTime = 0;
+            }
+            
+            _lastShootTime += deltaTime;
+        }
+
+        public void LateUpdate(float deltaTime)
+        { }
+
+        public void Dispose()
+        {
+            G.Instance.Services.GetService<IUpdateService>().Remove(this);
+        }
+    }
+}
---
+++ Assets/G/Scripts/ShootersLogic/IShooter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using G.Scripts.Services.Update;
+
+namespace G.Scripts.ShootersLogic
+{
+    public interface IShooter : IUpdatable, IDisposable
+    {
+        void Shoot();
+    }
+}
