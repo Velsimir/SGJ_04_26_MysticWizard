d17dbd2e873ba78eb448b76f9f886ae7f5502e46
Velsimir <nintendocoin@yandex.ru>
2026-05-02 12:23:43 +0300

    add bullet animation and sprite

---
+++ Assets/G/Scripts/BulletLogic/BulletRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace G.Scripts.BulletLogic
+{
+    public class BulletRotation : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 45f;
+        [SerializeField] private bool _clockwise = true;
+    
+        private void Update()
+        {
+            float direction = _clockwise ? -1f : 1f;
+            transform.Rotate(0f, 0f, _rotationSpeed * direction * Time.deltaTime);
+        }
+    }
+}
